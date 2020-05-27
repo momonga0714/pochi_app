572571812af9716c1ada7c2fd432dc8abb89e5dd
tomokitakenaga <tomoki0902@gmail.com>
2020-05-27 15:57:19 +0900

    セキュリティーアラートがなったため対策

---
+++ config/initializers/monkey_patches/javascript_helper.rb
@@ -0,0 +1,21 @@
+module ActionView::Helpers::JavaScriptHelper
+  # escape_javascriptをold_ejで呼び出せるように変更する
+　alias :old_ej :escape_javascript
+  # jをold_jで呼び出せるように変更する
+  alias :old_j :j
+　
+  def escape_javascript(javascript)
+  　# 引数のjavascriiptを文字に変換する
+　  javascript = javascript.to_s
+　　# もし、javascriptが空なら空白を返す
+    if javascript.empty?
+      result = ""
+    # そうでなければ、正規表現にマッチした文字をJS_ESCAPE_MAPで定義したマップに変換する
+    else
+      result = javascript.gsub(/(\\|<\/|\r\n|\342\200\250|\342\200\251|[\n\r"']|[`]|[$])/u, JS_ESCAPE_MAP)
+    end
+    javascript.html_safe? ? result.html_safe : result
+  end
+　# escape_javascriptをjで呼び出せるように変更する
+  alias :j :escape_javascript
+end
---
+++ config/initializers/monkey_patches/javascript_helper_test.rb
@@ -0,0 +1,7 @@
+ActionView::Helpers::JavaScriptHelper::JS_ESCAPE_MAP.merge!(
+  # "`" を "\\`"に,"$" を "\\$"に変換する
+　{
+    "`" => "\\`",
+    "$" => "\\$"
+  }
+)
