819785f10a22535932401fc60a529cf9393ca7da
tomokitakenaga <tomoki0902@gmail.com>
2020-05-27 15:41:32 +0900

    こんだてをpngで保存できる様に実装
    #what
    ランダムで生成されたこんだてを保存できる様に実装。
    pngで保存できる様に実装。
    #why
    ユーザーとして、お気に入りの献立や一週間のこだんてを固定したい場合において、機能としては不十分だったため、改善。
    ユーザーがこだんてを忘れずに、使用するには必須の機能だと考えたため。

---
+++ app/assets/javascripts/captcha.js
@@ -0,0 +1,15 @@
+//ロードされた際の処理として実施：
+window.onload = function(){
+
+  //HTML内に画像を表示
+  
+  //ボタンを押下した際にダウンロードする画像を作る
+  html2canvas(document.body,{
+    onrendered: function(canvas){
+      //aタグのhrefにキャプチャ画像のURLを設定
+      var imgData = canvas.toDataURL();
+      document.getElementById("ss").href = imgData;
+    }
+  });
+
+}
