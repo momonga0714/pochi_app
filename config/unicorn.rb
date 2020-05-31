764f926cdd2ee2131a395ea6350156be26330d35
tomokitakenaga <tomoki0902@gmail.com>
2020-06-01 08:21:58 +0900

    unicornファイルの変更

---
+++ config/unicorn.rb
@@ -0,0 +1,15 @@
+worker_processes 2
+working_directory "/home/anime/deploy_sample/current" #appと同じ階層を指定
+
+timeout 3600
+
+
+listen "/var/run/unicorn/Nginxで設定した名前.sock" ★
+pid "/var/run/unicorn/Nginxで設定した名前.pid"
+
+
+stderr_path "/home/anime/deploy_sample/current/log/unicorn.log"
+stdout_path "/home/anime/deploy_sample/current/log/unicorn.log"
+
+
+preload_app true
